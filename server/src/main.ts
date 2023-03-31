a08d4e03bc92f5b249a31bfe222906071e4198ab
csyun <csyun@creativelab-inc.com>
2023-03-31 18:04:52 +0900

    add: winston logger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { winstonLogger } from './utils/winston';
 
+const Port = 4000;
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: winstonLogger,
+  });
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  await app.listen(Port);
+  console.log(`Listening on Port ${Port}`);
 }
-bootstrap();
+bootstrap().catch((err) => {
+  winstonLogger.error(err);
+});
---
+++ server/src/utils/winston.ts
@@ -0,0 +1,46 @@
+/* istanbul ignore file */
+
+import { utilities, WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+
+const logLevel = process.env.LOG_LEVEL || 'info';
+const env = process.env.NODE_ENV;
+const appName = process.env.APP_NAME || 'Creative Wallet';
+
+export const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console({
+      level: logLevel,
+      // production 환경이라면 http, 개발환경이라면 모든 단계를 로그
+      format:
+        env !== 'production'
+          ? winston.format.combine(
+              winston.format.colorize(),
+              winston.format.timestamp(),
+              utilities.format.nestLike(appName, {
+                prettyPrint: true, // nest에서 제공하는 옵션. 로그 가독성을 높여줌
+              }),
+            )
+          : winston.format.json(),
+    }),
+  ],
+});
+
+export const winstonLogger = WinstonModule.createLogger({
+  transports: [
+    new winston.transports.Console({
+      level: logLevel,
+      // production 환경이라면 http, 개발환경이라면 모든 단계를 로그
+      format:
+        env !== 'production'
+          ? winston.format.combine(
+              winston.format.colorize(),
+              winston.format.timestamp(),
+              utilities.format.nestLike(appName, {
+                prettyPrint: true, // nest에서 제공하는 옵션. 로그 가독성을 높여줌
+              }),
+            )
+          : winston.format.json(),
+    }),
+  ],
+});
