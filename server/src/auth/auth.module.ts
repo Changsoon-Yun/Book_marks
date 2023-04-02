7b4ef55137ff5fa987c70c792d3f98d66ffee499
changsoonyun <changsoonyun@naver.com>
2023-04-03 00:59:42 +0900

    remove: unless files

--- server/src/auth/auth.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common";
-import { LoginModule } from "./login/login.module";
-import { SigninModule } from "./signin/signin.module";
-
-@Module({
-  imports: [LoginModule, SigninModule],
-})
-export class AuthModule {}
