0c99bede7ec4e02715ad077425cd72f2db225c2a
csyun <csyun@creativelab-inc.com>
2023-04-03 14:32:29 +0900

    feat: login

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,10 +1,22 @@
-import { Body, Controller, Post, ValidationPipe } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  ValidationPipe,
+} from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthCredentialDto } from "./dto/auth-credential.dto";
 
 @Controller("auth")
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @HttpCode(200)
+  @Post("/login")
+  async login(@Body(ValidationPipe) authCredentialDto: AuthCredentialDto) {
+    return this.authService.login(authCredentialDto);
+  }
 
   @Post("/signin")
   async signin(@Body(ValidationPipe) authCredentialDto: AuthCredentialDto) {
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -2,20 +2,26 @@ import {
   ConflictException,
   Injectable,
   InternalServerErrorException,
+  UnauthorizedException,
 } from "@nestjs/common";
 import { PrismaService } from "../prisma.service";
 import { AuthCredentialDto } from "./dto/auth-credential.dto";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
   async signup(authCredentialDto: AuthCredentialDto) {
-    // const { email, password } = authCredentialDto;
-    const email = authCredentialDto.email;
-    const password = authCredentialDto.password;
+    const { email, password } = authCredentialDto;
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     try {
-      const res = await this.prisma.user.create({ data: { email, password } });
+      const res = await this.prisma.user.create({
+        data: { email, password: hashedPassword },
+      });
       return res;
     } catch (err) {
       if (err.code === "P2002") {
@@ -23,6 +29,17 @@ export class AuthService {
       } else {
         throw new InternalServerErrorException();
       }
+    }
+  }
+
+  async login(authCredentialDto: AuthCredentialDto) {
+    const { email, password } = authCredentialDto;
+    const user = await this.prisma.user.findUnique({ where: { email } });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return "Login Success";
+    } else {
+      throw new UnauthorizedException("Login Failed");
     }
   }
 }
