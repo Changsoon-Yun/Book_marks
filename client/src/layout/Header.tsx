956df6052fc9886f54cab8db50866d364b0528a5
changsoonyun <changsoonyun@naver.com>
2023-04-05 23:48:11 +0900

    style: changed Header.tsx

--- client/src/layout/Header.tsx
+++ client/src/layout/Header.tsx
@@ -1,61 +1,133 @@
+'use client';
+
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+import { usePathname } from 'next/navigation';
 import { useUser } from '@/components/auth/hooks/useUser';
-import { useEffect, useState } from 'react';
 import { useAuth } from '@/components/auth/hooks/useAuth';
+import { useEffect, useState } from 'react';
+import { AccountCircle } from '@mui/icons-material';
+import { Menu, MenuItem, Typography } from '@mui/material';
 
 export default function Header() {
-  const { pathname } = useRouter();
-
+  const pathname = usePathname();
+  const [mounted, setMounted] = useState(false);
   const { user } = useUser();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [anchorEl2, setAnchorEl2] = useState<null | HTMLElement>(null);
+  // console.log(user);
   const { logout } = useAuth();
 
-  const [mounted, setMounted] = useState<boolean>(false);
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleMenu2 = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl2(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const handleClose2 = () => {
+    setAnchorEl2(null);
+  };
 
   useEffect(() => {
-    setMounted(() => true);
+    setMounted(true);
   }, []);
+
   return (
-    <Box>
+    <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+          <IconButton
+            aria-controls="menu-appbar"
+            size="large"
+            edge="start"
+            color="inherit"
+            onClick={handleMenu2}
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
             <MenuIcon />
           </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl2}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+            open={Boolean(anchorEl2)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose2}>
+              <Link href={'/auth/login'}>Login</Link>
+            </MenuItem>
+            <MenuItem onClick={handleClose2}>
+              <Link href={'/post'}>Post</Link>
+            </MenuItem>
+            {mounted && user && (
+              <MenuItem onClick={handleClose2}>
+                <Link href={'/post/write'}>Write</Link>
+              </MenuItem>
+            )}
+          </Menu>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            {pathname.substring(1).toLocaleUpperCase()}
+            {pathname}
           </Typography>
-          <Button color="inherit">
-            <Link shallow={true} href={'/post'}>
-              post
-            </Link>
-          </Button>
-          <Button color="inherit">
-            <Link shallow={true} href={'/post/write'}>
-              write
-            </Link>
-          </Button>
           {mounted && user && (
-            <Button onClick={logout} color="inherit">
-              logout
-            </Button>
-          )}
-          {mounted && !user && (
-            <Button color="inherit">
-              <Link shallow={true} href={'/auth/login'}>
-                Login
-              </Link>
-            </Button>
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="auth-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="auth-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+                <MenuItem onClick={logout}>Logout</MenuItem>
+              </Menu>
+            </div>
           )}
         </Toolbar>
       </AppBar>
     </Box>
   );
 }
+
+const Links = ({ href, text }: { href: string; text: string }) => {
+  return (
+    <Box sx={{ padding: '10px' }}>
+      <Link href={href}>{text}</Link>
+    </Box>
+  );
+};
