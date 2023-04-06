e367f3484578508f9066ad58ad3f6cac396f2039
changsoonyun <changsoonyun@naver.com>
2023-04-06 23:04:39 +0900

    testing husky

--- client/src/feature/post/hooks/useCreatePost.ts
+++ client/src/feature/post/hooks/useCreatePost.ts
@@ -1,38 +1,36 @@
-import { axiosInstance } from "@/lib/axios";
-import { useQuery } from "react-query";
-import { getJWTHeader } from "@/lib/axios/queryClient";
-import { useSetRecoilState } from "recoil";
-import { snackbarAtom } from "@/lib/recoil/atom";
-import axios from "axios";
-import { useRouter } from "next/router";
+import { axiosInstance } from '@/lib/axios';
+import { getJWTHeader } from '@/lib/axios/queryClient';
+import { useSetRecoilState } from 'recoil';
+import { snackbarAtom } from '@/lib/recoil/atom';
+import axios from 'axios';
+import { useRouter } from 'next/router';
 
 export type UserInput = {
   title: string;
   content: string;
 };
+
 export function useCreatePost() {
   const setSnack = useSetRecoilState(snackbarAtom);
   const router = useRouter();
 
   async function createPost(data: UserInput) {
     try {
-      const res = await axiosInstance.post("/post/write", data, {
+      const res = await axiosInstance.post('/post/write', data, {
         headers: getJWTHeader(),
       });
       if (res.status === 201) {
         setSnack({
           open: true,
-          text: "게시글이 생성되었습니다.",
-          severity: "success",
+          text: '게시글이 생성되었습니다.',
         });
-        return router.push("/post");
+        return router.push('/post');
       }
     } catch (err) {
       if (axios.isAxiosError(err) && err.response) {
         setSnack({
           open: true,
           text: err.response.data.message,
-          severity: "error",
         });
       }
     }
