340b6ea3ccb8ae9497de8747bb697e7474273e2f
csyun <csyun@creativelab-inc.com>
2023-04-11 14:03:46 +0900

    feat: add title to bookmark.dto.ts

--- server/src/bookmark/dto/bookmark.dto.ts
+++ server/src/bookmark/dto/bookmark.dto.ts
@@ -1,10 +1,15 @@
-import { IsString, IsUrl, MaxLength } from 'class-validator';
+import { IsString, IsUrl, MaxLength, MinLength } from 'class-validator';
 
 export class BookmarkDto {
   @IsUrl()
   url: string;
 
   @IsString()
+  @MinLength(1)
+  @MaxLength(30)
+  title: string;
+
+  @IsString()
   @MaxLength(200)
-  content: string;
+  description: string;
 }
