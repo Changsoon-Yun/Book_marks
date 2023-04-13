3f3dc8bd4afcde75d5a4e85b7965a91138214728
changsoonyun <changsoonyun@naver.com>
2023-04-13 22:55:24 +0900

    Add expiration date to setCookie function in cookie.ts

--- client/src/lib/cookie/cookie.ts
+++ client/src/lib/cookie/cookie.ts
@@ -5,7 +5,7 @@ export const COOKIE_NAME = 'bookmark';
 
 const cookies = new Cookies();
 export const setCookie = (name = COOKIE_NAME, value: UserWidthToken | undefined | null) => {
-  return cookies.set(name, value, { path: '/' });
+  return cookies.set(name, value, { path: '/', expires: new Date(Date.now() + 1000 * 60 * 60 * 24) });
 };
 
 export const getCookie = (name = COOKIE_NAME): UserWidthToken => {
