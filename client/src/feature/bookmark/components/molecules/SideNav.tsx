8b9881d64060688f59588132edfa9b12e80f8dc7
csyun <csyun@creativelab-inc.com>
2023-04-18 14:32:06 +0900

    Refactor SideNav component to use Chakra-UI styling and add folder icons.

--- client/src/feature/bookmark/components/molecules/SideNav.tsx
+++ client/src/feature/bookmark/components/molecules/SideNav.tsx
@@ -1,41 +1,76 @@
 import { useGetFolders } from '@/feature/bookmark/hooks/useGetFolders';
 import styled from '@emotion/styled';
 import { Folder } from '@/types/api/Folder';
-import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box } from '@chakra-ui/react';
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  Flex,
+  Icon,
+  Text,
+} from '@chakra-ui/react';
+import { AiFillFolder, AiFillFolderOpen } from 'react-icons/ai';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 
-function Item({ item }: { item: Folder }) {
+function Item({
+  item,
+  clickedFolder,
+  setClickedFolder,
+}: {
+  item: Folder;
+  clickedFolder: Folder;
+  setClickedFolder: Dispatch<SetStateAction<Folder>>;
+}) {
   const hasChildren = item.children && item.children.length > 0;
   return (
-    <AccordionItem>
-      <h2>
-        <AccordionButton pr={0} px={0}>
-          <Box as='span' flex='1' textAlign='left'>
-            {item.name}
-          </Box>
-          <AccordionIcon />
-        </AccordionButton>
-      </h2>
-      <AccordionPanel pb={4} px={0} pl={3}>
-        {hasChildren && (
-          <>
-            {item.children?.map((child) => (
-              <Item item={child} key={child.id} />
-            ))}
-          </>
-        )}
-      </AccordionPanel>
+    <AccordionItem border={'none'}>
+      {({ isExpanded }) => (
+        <>
+          <h2>
+            <AccordionButton
+              px={2}
+              bg={clickedFolder === item ? 'rgba(40, 87, 234, 0.3)' : 'inherit'}
+              _hover={{ bg: 'rgba(40,87,234,0.1)' }}
+              onClick={() => {
+                setClickedFolder(item);
+              }}
+              borderRadius={'10px'}>
+              <Flex flex='1' textAlign='left' align={'center'}>
+                <Icon as={isExpanded ? AiFillFolderOpen : AiFillFolder} />
+                <Text pl={2}>{item.name}</Text>
+              </Flex>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel px={0} py={0} pl={2} borderRadius={'10px'}>
+            {hasChildren && (
+              <>
+                {item.children?.map((child) => (
+                  <Item clickedFolder={clickedFolder} setClickedFolder={setClickedFolder} item={child} key={child.id} />
+                ))}
+              </>
+            )}
+          </AccordionPanel>
+        </>
+      )}
     </AccordionItem>
   );
 }
 
 export default function SideNav({ userName }: { userName: string | string[] }) {
   const { data: folders = [] } = useGetFolders(userName);
-  console.log(folders);
+  const [clickedFolder, setClickedFolder] = useState<Folder>(folders[0]);
+  useEffect(() => {
+    setClickedFolder(folders[0]);
+  }, [folders]);
   return (
-    <Box as={'nav'} w={'200px'} mr={'20px'}>
+    <Box as={'nav'} w={'250px'} mr={'20px'}>
       {folders.map((folder) => (
         <Accordion defaultIndex={[0]} allowMultiple key={folder.id}>
-          <Item item={folder} />
+          <Item item={folder} clickedFolder={clickedFolder} setClickedFolder={setClickedFolder} />
         </Accordion>
       ))}
     </Box>
